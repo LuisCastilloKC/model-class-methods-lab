6024eef0dd64c0279d77d431c5f360c9c8273084
Luis Castillo <mr.luiscastillo@gmail.com>
2021-05-18 22:39:58 -0400

    commit tree 46ab1b735e8dbb66bbbd2ae3727e72cf37000442

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,12 +2,16 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
-  def self.my_all
-    # all
+  def self.longest
+    longest = joins(:boats).merge(Boat.order("length DESC")).first
   end
 
+  def self.my_all
+    self.all
+  end
+  
   def self.longest
-    # Boat.longest.classifications
+    Boat.longest.classifications
   end
 
 end
